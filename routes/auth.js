c4107e9fcfad9b06682ef03e6485f9cd5a20fe4b
Javier Varela <jvarela@strands.com>
2020-05-16 18:59:59 +0200

    populate fans on updated user login

--- routes/auth.js
+++ routes/auth.js
@@ -80,7 +80,9 @@ router.post(
         await Location.findByIdAndUpdate(user.location, {
           $set: { coordinates: [lng, lat] }
         });
-        const updatedUser = await User.findById(user.id);
+        const updatedUser = await (
+          await (await User.findById(user.id)).populated("favs")
+        ).populate("fans");
         res.status(200).json(updatedUser);
       }
       return res.status(404).json({ code: "not-found" });
